18825c40d40ebc7cbcfa5e247e7c1f87cead7b08
Anderson Bairo <anderson.bairo@yahoo.com.br>
2018-01-21 20:59:46 -0200

    create gate and lamp models

--- apps/peripherals/models.py
+++ apps/peripherals/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class AbstractPeripheral(models.Model):
+    name = models.CharField(max_length=30)
+    pin = models.IntegerField()
+    value = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.value)
+
+
+class Gate(AbstractPeripheral):
+    pass
+
+class Lamp(AbstractPeripheral):
+    pass
